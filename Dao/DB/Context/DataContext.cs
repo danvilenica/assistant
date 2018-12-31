b8d3bbc7e8a826b8aac55d6e966c492abfbce28c
Danijel Vilenica <dvilenica@gmail.com>
2018-12-31 18:16:07 +0100

    migration

--- Dao/DB/Context/DataContext.cs
+++ Dao/DB/Context/DataContext.cs
@@ -14,16 +14,18 @@ namespace Dao.DB.Context
             : base(options)
         { }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<League> Leagues { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Match> Matches { get; set; }
         public DbSet<Player> Players { get; set; }
-        public DbSet<Team> Teams { get; set; }
         public DbSet<PlayerMatchStats> PlayerMatchStats { get; set; }
         public DbSet<Season> Seasons { get; set; }
         public DbSet<SeasonTeam> SeasonTeams{ get; set; }
         public DbSet<Stadium> Stadiums { get; set; }
+        public DbSet<Team> Teams { get; set; }
         public DbSet<TeamMatch> TeamMatches { get; set; }
         public DbSet<TeamPlayer> TeamPlayers { get; set; }
+        public DbSet<User> Users { get; set; }
+
     }
 }
---
+++ Dao/Migrations/20181231171557_added_tables.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dao.Migrations
+{
+    public partial class added_tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
